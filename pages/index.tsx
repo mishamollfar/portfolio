87dd66a78de60dad571fea53db627b97e60e5886
misha <tymish1@gmail.com>
2023-01-11 15:46:23 +0200

    initial project structure

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function HomePage() {
+    return (
+        <div>
+            <Head>
+                <title>Mykhailo Baran - Portfolio</title>
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+            </Head>
+            <h1>Home Page</h1>
+        </div>
+    );
+}
---
+++ pages/projects.tsx
@@ -0,0 +1,5 @@
+export default function ProjectPage() {
+    return (
+        <h1>Projects page</h1>
+    );
+}
