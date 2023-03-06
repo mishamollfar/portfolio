55a99ec2c2e7fdc9e3b33e32b6ee613e795ea528
misha <tymish1@gmail.com>
2023-03-06 12:49:30 +0200

    add new component text-sphere

---
+++ components/text-sphere.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useEffect } from 'react';
+import TagCloud from 'TagCloud';
+
+export default function TextSphere() {
+    useEffect(() => {
+        return () => {
+            const container: string = '.tagcloud';
+
+            const text: string[] = [
+                'HTML',
+                'CSS',
+                'SASS',
+                'JavaScript',
+                'Angular 2+',
+                'React',
+                'Node.js',
+                'Next.js',
+                'MongoDB',
+                'ES6',
+                'GIT',
+                'GITHUB',
+                'Rxjs',
+                'C++',
+                'Python',
+                'Pascal',
+                '.pug',
+                'Webpack',
+                'npm',
+                'VS Code',
+                'WebStorm'
+            ];
+
+            const options = {
+                radius: 300,
+                maxSpeed: 'normal',
+                initialSpeed: 'normal',
+                keep: true,
+            };
+
+            TagCloud(container, text, options);
+        }
+    }, []);
+
+    return (
+        <div className='text-sphere flex relative top-0 w-full h-full items-center justify-center'>
+            <span className='tagcloud text-xl font-medium tracking-wider text-cyan-300'></span>
+        </div>
+    );
+}
