8b8c2f9dcdf38168b4233176bcb52f317a6bc7aa
misha <tymish1@gmail.com>
2023-03-09 09:06:22 +0200

    add style to active sidebar button

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,15 +1,32 @@
+'use client'
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Sidebar() {
+    const pathname = usePathname();
     return (
         <nav className="flex flex-row items-center justify-end text-base">
-            <Link className="text-lg transition duration-100 ease-in-out hover:text-sky-400 mr-4" href="/">Home</Link>
-            <Link className="text-lg transition duration-100 ease-in-out hover:text-sky-400 mr-4"
-                  href="/projects">Projects</Link>
-            <Link className="text-lg transition duration-100 ease-in-out hover:text-sky-400 mr-4"
-                  href="/about">About</Link>
-            <Link className="text-lg transition duration-100 ease-in-out hover:text-sky-400 mr-4"
-                  href="/contact">Contact</Link>
+            <Link
+                className={ `text-lg transition duration-100 ease-in-out navigation-button ${ pathname === '/' ? 'active-nav-btn' : '' }` }
+                href={ { pathname: '/' } }>
+                Home
+            </Link>
+            <Link
+                className={ `text-lg transition duration-100 ease-in-out navigation-button ${ pathname === '/projects' ? 'active-nav-btn' : '' }` }
+                href={ { pathname: '/projects' } }>
+                Projects
+            </Link>
+            <Link
+                className={ `text-lg transition duration-100 ease-in-out navigation-button ${ pathname === '/about' ? 'active-nav-btn' : '' }` }
+                href={ { pathname: '/about' } }>
+                About
+            </Link>
+            <Link
+                className={ `text-lg transition duration-100 ease-in-out navigation-button ${ pathname === '/contact' ? 'active-nav-btn' : '' }` }
+                href={ { pathname: '/contact' } }>
+                Contact
+            </Link>
         </nav>
     );
 }
